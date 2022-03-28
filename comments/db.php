224f3f8c036c7b59b3dc7b98edac0604999ac3aa
minionsii <102497117+minionsii@users.noreply.github.com>
2022-03-28 09:58:40 +0700

    demo

--- comments/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbconn = mysqli_connect("localhost","root","","muabannongsan");
-if(!$dbconn){
-	echo "connect fail";
-}
-else{
-	mysqli_set_charset($dbconn,'utf8');
-}
-?>
--- danghinhanh/database_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//database_connection.php
-
-$dbconnect = new PDO('mysql:host=localhost;dbname=testing2', 'root', '');
-
-?>
